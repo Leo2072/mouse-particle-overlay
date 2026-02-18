80fafe58c839f3cb702e46b830fbb0957f7e32c3
HeadfirstMissileCollider <leotuananhnguyen@gmail.com>
2026-02-18 15:32:57 -0700

    Updated to Godot 4.6.1. Reworked the Linux version to only use X11 Windows, and to explicitly set the mouse passthrough polygons using the Game Engine's implementation in case it does not do so when the program is run on Wayland.

--- CrossPlatform/FullPassthroughSetter.cs
+++ CrossPlatform/FullPassthroughSetter.cs
@@ -2,6 +2,11 @@ using Godot;
 #if GODOT_WINDOWS
 using System;
 using System.Runtime.InteropServices;
+#elif GODOT_LINUXBSD
+using DisplayHandle = nint;
+using WindowHandle = ulong;
+using RegionHandle = nint;
+using System.Runtime.InteropServices;
 #endif
 
 
@@ -13,7 +18,7 @@ using System.Runtime.InteropServices;
 /// 
 /// <b>NOTE:</b>
 /// <br/>
-/// This functionality is only supported on Windows, Linux (X11), and MacOS.
+/// This functionality is only supported for Windows, Linux (X11), and MacOS Desktop Windows.
 /// <br/>
 /// This functionality is only available for Borderless <c>Window</c>s.
 /// <para/>
@@ -87,7 +92,8 @@ public partial class FullPassthroughSetter : RefCounted
     /// <param name="Target">The <c>Window</c> to modify.</param>
     public static void EnableFullPassthrough(Window Target)
     {
-        IntPtr hwnd = (IntPtr)DisplayServer.WindowGetNativeHandle(
+        IntPtr hwnd = (IntPtr)DisplayServer.WindowGetNativeHandle
+        (
             DisplayServer.HandleType.WindowHandle, Target.GetWindowId()
         );
         UInt32 currentExStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
@@ -126,7 +132,8 @@ public partial class FullPassthroughSetter : RefCounted
     /// <param name="Target">The <c>Window</c> to modify.</param>
     public static void DisableFullPassthrough(Window Target)
     {
-        IntPtr hwnd = (IntPtr)DisplayServer.WindowGetNativeHandle(
+        IntPtr hwnd = (IntPtr)DisplayServer.WindowGetNativeHandle
+        (
             DisplayServer.HandleType.WindowHandle, Target.GetWindowId()
         );
         UInt32 currentExStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
@@ -139,11 +146,158 @@ public partial class FullPassthroughSetter : RefCounted
 
 
 
+    #elif GODOT_LINUXBSD
+#region LINUX_IMPLEMENTATION
+    // On Linux, parts of the Window outside of its Window Region are still rendered,
+    // so we only need to set said Region to an Empty Polygon.
+
+    // Additional care must be taken as the engine does not automatically call
+    // the update region code when running on Wayland, even for X11 Windows.
+
+
+    // Import the LibX11 and LibXext API methods to Create, Set, and Destroy Window Regions.
+    // Source: https://github.com/godotengine/godot/blob/master/platform/linuxbsd/x11/display_server_x11.cpp#L2265
+    [DllImport("libX11.so", EntryPoint = "XCreateRegion", SetLastError = true)]
+    public static extern RegionHandle CreateRegion();
+
+
+    [DllImport("libX11.so", EntryPoint = "XDestroyRegion", SetLastError = true)]
+    public static extern void DestroyRegion(RegionHandle Target);
+
+
+    [DllImport("libXext.so", EntryPoint = "XShapeCombineRegion", SetLastError = true)]
+    private static extern void CombineWindowInputRegion
+    (
+        DisplayHandle DisplayPtr, WindowHandle Target, int DestinationKind,
+        int OffsetX, int OffsetY, RegionHandle InputRegion, int CombineOpCode
+    );
+
+    
+    [DllImport("libXext.so", EntryPoint = "XShapeCombineMask", SetLastError = true)]
+    private static extern void CombineWindowInputMask
+    (
+        DisplayHandle DisplayPtr, WindowHandle Target, int DestinationKind,
+        int OffsetX, int OffsetY, RegionHandle InputRegion, int CombineOpCode
+    );
+
+
+    // Enumeration to specify that the destination region is written to.
+    const int ShapeInputDest = 2;
+
+    // Enumeration to specify that the shape of the first region
+    // is set to that of the second one in the shape operation.
+    const int ShapeSetOp = 0;
+
+
+    /// <summary>
+    /// Make mouse inputs completely pass through and ignored by
+    /// the <paramref name="Target"/>, without affecting its visibility.
+    /// <br/>
+    /// To make the <paramref name="Target"/> able to intercept mouse inputs again,
+    /// use <c>DisableFullPassthrough</c>.
+    /// <para/>
+    /// 
+    /// <b>NOTE:</b>
+    /// <br/>
+    /// This method is only supported on Windows, Linux (X11), and MacOS.
+    /// <para/>
+    /// <b>Windows</b>:
+    /// This method will set the <c>WS_EX_TRANSPARENT</c> Extended Window Style flag
+    /// of the <paramref name="Target"/>.
+    /// Thus, the user should be careful when setting the Extended Window Style flags
+    /// for <c>Window</c>s that will use this method.
+    /// <para/>
+    /// <b>Linux</b> and <b>MacOS</b>:
+    /// This method will set the <c>MousePassthroughPolygon</c> of the <paramref name="Target"/>.
+    /// Thus, this method should not be used for <c>Window</c>s that will have their
+    /// <c>MousePassthroughPolygon</c> or <c>MousePassthrough</c> fields controlled by other sources.
+    /// </summary>
+    /// 
+    /// <param name="Target">The <c>Window</c> to modify.</param>
+    public static void EnableFullPassthrough(Window Target)
+    {
+        // Set the passthrough polygon to a region outside of the Bounds of the Windows
+        // so that no mouse clicks will fall into it.
+        Target.MousePassthroughPolygon = [new(-1, -1), new(-1, -1), new(-1, -1)];
+
+        // Additionally set the input region using the engine source code,
+        // as the mouse passthrough polygon has no effects for X11 windows
+        // when the device runs on Wayland (for some reasons).
+        int WindowID = Target.GetWindowId();
+        RegionHandle EmptyRegion = CreateRegion();
+	    CombineWindowInputRegion
+        (
+            (DisplayHandle)DisplayServer.WindowGetNativeHandle
+            (
+                DisplayServer.HandleType.DisplayHandle, WindowID
+            ),
+            (WindowHandle)DisplayServer.WindowGetNativeHandle
+            (
+                DisplayServer.HandleType.WindowHandle, WindowID
+            ),
+            ShapeInputDest, 0, 0, EmptyRegion, ShapeSetOp
+        );
+	    DestroyRegion(EmptyRegion);
+        return;
+    }
+
+
+    /// <summary>
+    /// Make the <paramref name="Target"/> intercept mouse inputs again.
+    /// <para/>
+    /// 
+    /// <b>NOTE:</b>
+    /// <br/>
+    /// This method is only supported on Windows, Linux (X11), and MacOS.
+    /// <para/>
+    /// <b>Windows</b>:
+    /// This method will set the <c>WS_EX_TRANSPARENT</c> Extended Window Style flag
+    /// of the <paramref name="Target"/>.
+    /// Thus, the user should be careful when setting the Extended Window Style flags
+    /// for <c>Window</c>s that will use this method.
+    /// <para/>
+    /// <b>Windows</b>:
+    /// Parts of the <paramref name="Target"/> outside of its <c>MousePassthroughPolygon</c>
+    /// will still not intercept mouse inputs.
+    /// <para/>
+    /// <b>Linux</b> and <b>MacOS</b>:
+    /// This method will clear the <c>MousePassthroughPolygon</c> of the <paramref name="Target"/>
+    /// and make all parts of it intercept mouse inputs again.
+    /// Thus, this method should not be used for <c>Window</c>s that will have their
+    /// <c>MousePassthroughPolygon</c> or <c>MousePassthrough</c> fields controlled by other sources.
+    /// </summary>
+    /// 
+    /// <param name="Target">The <c>Window</c> to modify.</param>
+    public static void DisableFullPassthrough(Window Target)
+    {
+        // Clear the passthrough polygon so that it falls back to the default (covers the whole window).
+        Target.MousePassthroughPolygon = [];
+
+        // Additionally set the input region using the engine source code,
+        // as the mouse passthrough polygon has no effects for X11 windows
+        // when the device runs on Wayland (for some reasons).
+        int WindowID = Target.GetWindowId();
+    	CombineWindowInputMask
+        (
+            (DisplayHandle)DisplayServer.WindowGetNativeHandle
+            (
+                DisplayServer.HandleType.DisplayHandle, WindowID
+            ),
+            (WindowHandle)DisplayServer.WindowGetNativeHandle
+            (
+                DisplayServer.HandleType.WindowHandle, WindowID
+            ),
+            ShapeInputDest, 0, 0, 0, ShapeSetOp
+        );
+        return;
+    }
+#endregion
+
 
 
     #else
 #region OTHER_IMPLEMENTATIONS
-    // On every other platform, parts of the Window outside of its Window Region are still rendered,
+    // On MacOS, parts of the Window outside of its Window Region are still rendered,
     // so we only need to set said Region to an Empty Polygon.
 
     /// <summary>
@@ -173,7 +327,7 @@ public partial class FullPassthroughSetter : RefCounted
     /// <param name="Target">The <c>Window</c> to modify.</param>
     public static void EnableFullPassthrough(Window Target)
     {
-        #if !(GODOT_MACOS || GODOT_LINUXBSD)
+        #if !GODOT_MACOS
         // The Window Region (MousePassthroughPolygon) is only implemented for Windows, MacOS, and Linux(X11).
         // For all other platforms, it may have no effects.
         GD.PushWarning("Mouse Passthrough may not be implemented on your platform.");
@@ -181,8 +335,7 @@ public partial class FullPassthroughSetter : RefCounted
 
         // Set the passthrough polygon to a region outside of the Bounds of the Windows
         // so that no mouse clicks will fall into it.
-        Vector2 InvalidVertex = new(-1, -1);
-        Target.MousePassthroughPolygon = [InvalidVertex, InvalidVertex, InvalidVertex];
+        Target.MousePassthroughPolygon = [new(-1, -1), new(-1, -1), new(-1, -1)];
         return;
     }
 
